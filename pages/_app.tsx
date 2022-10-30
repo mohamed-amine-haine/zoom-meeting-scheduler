40475129147db3153966de0d7ba36e06606bcfe0
Mohamed Amine HAINE <haine.mohamed.amine@gmail.com>
2022-10-30 21:42:25 +0100

    Fix timezone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone'; // dependent on utc plugin
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const queryClient = new QueryClient();
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,24 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for selectable
 import styles from '../styles/Home.module.css';
 import { useEvents } from '../hooks/useEvents';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 export default function Home() {
   const { events, addEvent, openEvent } = useEvents();
+
+  const [timezone, setTimezone] = useState<string>();
+
+  useEffect(() => {
+    setTimezone(dayjs.tz.guess());
+  }, []);
 
   return (
     <div className={styles.container}>
       <FullCalendar
         plugins={[timeGridPlugin, interactionPlugin]}
         initialView="timeGridWeek"
-        timeZone="local"
+        timeZone={timezone}
         locale="fr"
         selectable
         select={addEvent}
