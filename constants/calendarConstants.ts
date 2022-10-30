cfbeb902a7528a04e07847dd7ed5f0b6fabf3a03
Mohamed Amine HAINE <haine.mohamed.amine@gmail.com>
2022-10-30 13:11:27 +0100

    Add select and create event on calendar without create zoom meeting

---
+++ constants/calendarConstants.ts
@@ -0,0 +1,10 @@
+import { FormatRangeOptions } from '@fullcalendar/react';
+
+export const FULLCALENDAR_LOCALE = 'fr';
+
+export const FULLCALENDAR_DATE_TIME_RANGE_FORMAT: FormatRangeOptions = {
+  locale: FULLCALENDAR_LOCALE,
+  dateStyle: 'short',
+  timeStyle: 'short',
+  separator: ' => ',
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import FullCalendar from '@fullcalendar/react';
+import FullCalendar, { DateSelectArg, EventInput } from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for selectable
 import styles from '../styles/Home.module.css';
+import { useCallback, useState } from 'react';
+import { promptEventObjectForm } from '../services/calendarServices';
 
 export default function Home() {
+  const [events, setEvents] = useState<EventInput[]>([]);
+
+  const addEvent = useCallback(({ start, end }: DateSelectArg) => {
+    const title = promptEventObjectForm(start, end);
+
+    if (title) {
+      const newEvent: EventInput = { start, end, title };
+      setEvents(oldEvents => [...oldEvents, newEvent]);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
-      <FullCalendar plugins={[timeGridPlugin]} initialView="timeGridWeek" />
+      <FullCalendar
+        plugins={[timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        timeZone="local"
+        locale="fr"
+        selectable
+        select={addEvent}
+        events={events}
+      />
     </div>
   );
 }
