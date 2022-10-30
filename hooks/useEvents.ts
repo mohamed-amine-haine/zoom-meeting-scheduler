04a5555ffd71985122eeef4d607922300828692e
Mohamed Amine HAINE <haine.mohamed.amine@gmail.com>
2022-10-30 18:08:37 +0100

    refacto addEvent in useEvents hook

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -11,28 +11,27 @@ export const useEvents = (eventsDefaultValue: CalendarEvent[] = []) => {
 
   const addEvent = useCallback(async ({ start, end }: DateSelectArg) => {
     const title = promptEventObjectForm(start, end);
+    if (!title) {
+      return;
+    }
 
-    if (title) {
-      const [result, error] = await asyncTryCatch(
-        async () =>
-          await createZoomMeeting({
-            title,
-            start,
-            end,
-          })
-      );
+    const [result, error] = await asyncTryCatch(
+      async () =>
+        await createZoomMeeting({
+          title,
+          start,
+          end,
+        })
+    );
 
-      if (error) {
-        toast.error(
-          `Zoom Meeting Not Created!\n${error?.response?.data?.error}`
-        );
-        return;
-      }
+    if (error) {
+      toast.error(`Zoom Meeting Not Created!\n${error?.response?.data?.error}`);
+      return;
+    }
 
-      if (result.data) {
-        setEvents(oldEvents => [...oldEvents, result.data]);
-        toast.success('Zoom Meeting Created!');
-      }
+    if (result.data) {
+      setEvents(oldEvents => [...oldEvents, result.data]);
+      toast.success('Zoom Meeting Created!');
     }
   }, []);
 
