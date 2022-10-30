ec6f971ae9447a8c11a7cfa303e8dfa3419efeb8
Mohamed Amine HAINE <haine.mohamed.amine@gmail.com>
2022-10-30 23:09:05 +0100

    Fix formatDateTimeToZoomFormat

--- constants/zoomMeetingConstants.ts
+++ constants/zoomMeetingConstants.ts
@@ -1,3 +1,1 @@
-export const ZOOM_DATE_FORMAT = 'YYYY-MM-DD[T]HH:mm:ssZ';
-
 export const ZOOM_MEETINGS_CACHE_KEYS = 'zoom_meetings';
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         select={addEvent}
         events={events}
         eventClick={openEvent}
+        allDaySlot={false}
       />
     </div>
   );
