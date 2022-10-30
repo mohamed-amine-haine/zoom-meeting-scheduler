70499c1ac18e23f4bce644717797f652fb3ba852
Mohamed Amine HAINE <haine.mohamed.amine@gmail.com>
2022-10-30 12:02:39 +0100

    Add fullcalendar day grid

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ntm = require('next-transpile-modules');
+
+const withTM = ntm([
+  '@fullcalendar/common',
+  '@fullcalendar/react',
+  '@fullcalendar/daygrid',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
